271dd52fd25309283cdf0546fd43c0c413e514b3
Yumi Patton <yumipatton@gmail.com>
2020-10-14 23:36:39 +1100

    before merge

--- js/product-list.js
+++ js/product-list.js
@@ -1,3 +1,62 @@
 export function greeting() {
   console.log('hello world!');
 }
+
+const productList = [
+  {
+    productId: 1,
+    productName: 'Fancy Chocolate cake',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+  {
+    productId: 2,
+    productName: 'Fancy Chocolate cake 2',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+  {
+    productId: 3,
+    productName: 'Fancy Chocolate cake',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+  {
+    productId: 4,
+    productName: 'Fancy Chocolate cake 2',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+  {
+    productId: 5,
+    productName: 'Fancy Chocolate cake',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+  {
+    productId: 6,
+    productName: 'Fancy Chocolate cake 2',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+  {
+    productId: 7,
+    productName: 'Fancy Chocolate cake',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+  {
+    productId: 8,
+    productName: 'Fancy Chocolate cake 2',
+    price: '$10.44',
+    image: 'https://images.unsplash.com/photo-1586985289906-406988974504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    category: 'cake',
+  },
+];
