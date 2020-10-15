c5be869a8d415ccc6b845512aac490c55ac16fe8
Yumi Patton <yumipatton@gmail.com>
2020-10-16 00:39:05 +1100

    fixed npm run build

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-import {productList, displayProducts} from './product-list.js';
+import { productList, displayProducts } from './product-list.js';
+import filterToggle from './filterToggle.js';//Toggling to a dropdown menu when px
 
+filterToggle();
 displayProducts(productList);
 
-//testing whether the add-to-cart button can retrieve the info
-document.querySelector(".product-cards").addEventListener( "click", e => {
-    console.log(
-      e.target.closest('.product-card').dataset.id,
-      e.target.closest('.product-card').dataset.category,
-      e.target.closest('.product-card').querySelector('.product-card-name').innerText,
-      e.target.closest('.product-card').querySelector('.product-card-price').innerText,
-    );
+//Dummy function! this is just testing whether the add-to-cart button can retrieve the info
+document.querySelector('.product-cards').addEventListener('click', (e) => {
+  console.log(
+    e.target.closest('.product-card').dataset.id,
+    e.target.closest('.product-card').dataset.category,
+    e.target.closest('.product-card').querySelector('.product-card-name').innerText,
+    e.target.closest('.product-card').querySelector('.product-card-price').innerText,
+  );
 });
